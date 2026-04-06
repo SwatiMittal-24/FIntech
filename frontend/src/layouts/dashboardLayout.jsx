b37f1db894d27ef71629de9a32f83a9e6efb8d2f
Swati <swatimittal7819@gmail.com>
2026-04-06 18:14:42 +0530

    fix deployment issues

--- frontend/src/layouts/dashboardLayout.jsx
+++ frontend/src/layouts/dashboardLayout.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import Sidebar from "../components/sidebar";
+import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 
 export default function DashboardLayout() {
